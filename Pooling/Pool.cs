cf8783d7ec9138bb4f7b32c8e6bd6cc7c3913ff5
medzumi <pkkudrin@gmail.com>
2022-05-02 03:45:16 +0300

    save

--- Pooling/Pool.cs
+++ Pooling/Pool.cs
@@ -7,6 +7,8 @@ namespace Utilities.Pooling
     {
         private readonly Stack<T> _pool;
         private readonly Func<T> _createFunc;
+        private Action<T> _resolveAction;
+        private Action<T> _releaseAction;
 
         public Pool(int capacity, Func<T> createFunc)
         {
@@ -17,14 +19,30 @@ namespace Utilities.Pooling
                 _pool.Push(createFunc.Invoke());
             }
         }
+        
+        public Pool(int capacity, Func<T> createFunc, Action<T> resolveAction, Action<T> releaseAction)
+        {
+            _pool = new Stack<T>(capacity);
+            _createFunc = createFunc;
+            for (int i = 0; i < capacity; i++)
+            {
+                _pool.Push(createFunc.Invoke());
+            }
+
+            _releaseAction = releaseAction;
+            _resolveAction = resolveAction;
+        }
 
         public T Get()
         {
-            return _pool.Count > 0 ? _pool.Pop() : _createFunc.Invoke();
+            var result = _pool.Count > 0 ? _pool.Pop() : _createFunc.Invoke();
+            _resolveAction?.Invoke(result);
+            return result;
         }
 
         public void Release(T tObject)
         {
+            _releaseAction?.Invoke(tObject);
             _pool.Push(tObject);
         }
     }
